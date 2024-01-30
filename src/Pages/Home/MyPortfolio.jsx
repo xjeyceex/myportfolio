14b374d76481d06e21165beff2413ebc719b847b
Jc Miguel <jcmiguel.beltran@gmail.com>
2024-01-30 16:20:00 +0800

    added channel name

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -5,8 +5,7 @@ export default function MyPortfolio() {
     <section className="portfolio--section" id="MyPortfolio">
       <div className="portfolio--container-box">
         <div className="portfolio--container">
-          <p className="sub--title">Recent Projects</p>
-          <h2 className="section--heading">My Portfolio</h2>
+          <h2 className="section--heading">Recent Projects</h2>
         </div>
       </div>
       <div className="portfolio--section--container">
